b0d2944008290339ae16cf5e2d34ead7dc52177e
Md Adil <adil.sudo@gmail.com>
2023-10-17 22:50:19 +0530

    update plugin

--- packages/app/src/fastify/plugins.ts
+++ packages/app/src/fastify/plugins.ts
@@ -1,12 +1,14 @@
 import { wrap } from "../context.js";
-import fp from "fastify-plugin";
 import type { App } from "../types.js";
 import { handleResponse } from "../response.js";
 import { triggerOnSent } from "../hooks.js";
+
 type CF = CallableFunction;
-export const appPlugin = fp(function appPlugin(fastify: App, _: {}, next: CF) {
+
+export const appPlugin = function minimajs(fastify: App, _: {}, next: CF) {
   fastify.addHook("onRequest", wrap);
   fastify.addHook("preSerialization", handleResponse);
   fastify.addHook("onResponse", triggerOnSent);
   next();
-});
+};
+(appPlugin as any)[Symbol.for("skip-override")] = true;
