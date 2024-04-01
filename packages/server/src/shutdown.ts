3d2be25e344f22476e23264e05ea3abf4d98681d
Md Adil <md-adil@live.com>
2024-04-01 16:51:10 +0530

    prevent sigkil

--- packages/server/src/shutdown.ts
+++ packages/server/src/shutdown.ts
@@ -2,14 +2,14 @@ import type { App } from "./types.js";
 type Signal = NodeJS.Signals;
 
 export function shutdownListener(app: App, killSignal: Signal[]) {
-  let hasBeenTriggered = false;
   async function quit(sig: Signal) {
-    if (hasBeenTriggered) return;
-    hasBeenTriggered = true;
     app.log.info(`${sig}: %s`, "closing server");
     await app.close();
     app.log.info("finished all requests");
-    process.kill(process.pid, sig);
+    // check if same signals are not attached again then quit the app.
+    if (process.listeners(sig).filter((x) => x !== quit).length === 0) {
+      process.kill(process.pid, sig);
+    }
   }
   killSignal.forEach((signal) => process.on(signal, quit));
 }
