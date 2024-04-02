fc8bb0fc75a0c0eab74a19de98ca291b3b612719
Md Adil <md-adil@live.com>
2024-04-02 13:52:38 +0530

    fix gracefull

--- packages/server/src/shutdown.ts
+++ packages/server/src/shutdown.ts
@@ -3,13 +3,21 @@ type Signal = NodeJS.Signals;
 
 export function shutdownListener(app: App, killSignal: Signal[]) {
   async function quit(sig: Signal) {
-    app.log.info(`${sig}: %s`, "closing server");
+    app.log.info(`%s: closing server`, sig);
+    console.time("server closed");
     await app.close();
-    app.log.info("finished all requests");
+    console.timeEnd("server closed");
+    next(sig);
+  }
+
+  function next(sig: Signal) {
+    // removing existing listener, otherwise function quite might execute again by process.kill function.
+    process.off(sig, quit);
     // check if same signals are not attached again then quit the app.
-    if (process.listeners(sig).filter((x) => x !== quit).length === 0) {
+    if (process.listeners(sig).length === 0) {
       process.kill(process.pid, sig);
     }
   }
+
   killSignal.forEach((signal) => process.on(signal, quit));
 }
