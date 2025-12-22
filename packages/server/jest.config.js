feb5485d55d145406d5464ce3d5043576808f019
Md Adil <md-adil@live.com>
2025-12-22 17:34:57 +0530

    coverage settings

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -3,7 +3,8 @@ import { jestConfig } from "../config/jest.config.mjs";
 /** @type {import('jest').Config} */
 export default {
   ...jestConfig,
-  coverageReporters: ["html"],
+  coveragePathIgnorePatterns: ["__tests__/", "/node_modules/", "src/mock/"],
+
   testTimeout: 10000,
   forceExit: true,
 };
